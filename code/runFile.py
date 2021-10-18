b2234e06e5af1e115a5721bcc8787043d0e208e7
Stephen <stephen@umw.edu>
2021-10-18 15:31:11 -0400

    Compute max axes limits explicitly.

--- code/runFile.py
+++ code/runFile.py
@@ -2,10 +2,10 @@ from bvm_suite import *
 from bvm_sweep import *
 import pandas as pd
 suite = bvmSuite({"p":.3, "o":.20, "d":.60,"issues":3, "l_steps":1000, "n_agents":50}, 100)
-suite.run()
-x = suite.getData()
-x.to_csv('suiteData.csv')
-#x = pd.read_csv('suiteData.csv')
+#suite.run()
+#x = suite.getData()
+#x.to_csv('suiteData.csv')
+x = pd.read_csv('suiteData.csv')
 
 print(x)
 
